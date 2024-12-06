c85c539c008114efd4d999cbb7d66b782a3cc7c8
Seliudev <Seliudev@DESKTOP-D0QT1F4>
2024-12-07 00:00:44 +0300

    Bootcamp2.3

--- BLL/Services/ArtworksService.cs
+++ BLL/Services/ArtworksService.cs
@@ -32,11 +32,18 @@ namespace BLL.Services
                 return Error("Artworks with the same name and description exist!");
             record.Title = record.Title?.Trim();
             _db.Artworks.Add(record);
+            _db.SaveChanges();
+            return Success("Artwork is created successfully!");
         }
 
         public ServiceBase Delete(int id)
         {
-            throw new NotImplementedException();
+            var entity = _db.Artworks.SingleOrDefault(w => w.Id == id);
+            if (entity is null)
+                return Error("Artwork can't be found!");
+            _db.Artworks.Remove(entity);
+            _db.SaveChanges();
+            return Success("Artwork is deleted successfully!");
         }
 
         public IQueryable<ArtworksModel> Query()
@@ -46,7 +53,12 @@ namespace BLL.Services
 
         public ServiceBase Update(Artworks record)
         {
-            throw new NotImplementedException();
+            if (_db.Artworks.Any(w => w.Title.ToLower() == record.Title.ToLower().Trim() && w.Description.ToLower() == record.Description.ToLower()))
+                return Error("Artworks with the same name and description exist!");
+            record.Title = record.Title?.Trim();
+            _db.Artworks.Update(record);
+            _db.SaveChanges();
+            return Success("Artwork is updated successfully!");
         }
     }
 }
